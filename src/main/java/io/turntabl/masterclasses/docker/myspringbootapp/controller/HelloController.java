b343ef7bacdae0a8f0ce7aacd479cdbb6564eb5b
Ri <riyanat@Riyanats-MacBook-Pro.local>
2023-11-30 13:46:56 +0000

    Add Hello Controller.

---
+++ src/main/java/io/turntabl/masterclasses/docker/myspringbootapp/controller/HelloController.java
@@ -0,0 +1,20 @@
+package io.turntabl.masterclasses.docker.myspringbootapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+    private final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/")
+    public String sayHello() {
+        logger.info("saying hello");
+        return "Hello There!";
+     }
+}
